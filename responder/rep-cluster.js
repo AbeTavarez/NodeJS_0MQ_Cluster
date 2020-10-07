85c62e872e75313eed52c90ccdd3be6ed5b3db23
Abraham E. Tavarez <abetavarez@icloud.com>
2020-10-07 17:53:45 -0400

    cluster inport

--- responder/rep-cluster.js
+++ responder/rep-cluster.js
@@ -1,9 +1,13 @@
 `use strict`;
 
-const { cluster } = require(`cluster`);
+const cluster = require(`cluster`);
 const { fs } = require(`fs`);
 const zmq = require(`zeromq`);
 const numWorkers = require(`os`).cpus().length;
 
-//* Responder Cluster
 console.log(numWorkers);
+
+//* Responder Cluster
+if (cluster.isMaster) {
+  console.log(`master`);
+}
